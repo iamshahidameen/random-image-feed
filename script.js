a9a58cbbbc4d941731c1e80dcff7dfcbc7d54068
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-03-25 01:28:20 +0500

    Added js to populate the images from the loop

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const UNSPLASH_URL = 'https://source.unsplash.com/random/'
+const UNSPLASH_URL = 'https://source.unsplash.com/random/';
+const galleryWrap = document.querySelector('.gallery-wrapper');
+const gridRows = 4*5;
+for(let i = 1; i<=gridRows; i++) {
+
+    const imgWrap = document.createElement('div');
+    imgWrap.classList.add('img-wrapper');
+    imgWrap.innerHTML =`
+    <img src="https://source.unsplash.com/random/${randomSizeGenerator()}" alt="">
+    `
+
+    // galleryWrap.innerHTML = `
+    // <div class="img-wrapper">
+    //     
+    // </div>
+    // `
+    galleryWrap.appendChild(imgWrap);
+}
+function randomSizeGenerator(){
+    return `${randomNumberGen()}x${randomNumberGen()}`
+}
+function randomNumberGen(){
+   return Math.floor(Math.random()*20 )+300;
+}
